c7845b54534683f2cc69370f532e10754e22b552
GabrielVader759 <gabrieltorresbrmg@gmail.com>
2025-06-14 18:15:11 -0300

    Barreira Funcional
    A barreira tem vida agora

---
+++ Assets/Scripts/VidaBarreira.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class VidaBarreira : MonoBehaviour
+{
+    public int vida = 20;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerFire" || other.gameObject.tag == "EnemyFire")
+        {
+            vida--;
+            if(vida == 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
